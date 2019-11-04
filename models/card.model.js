3d989490034d40bd42102d8f9b12742558f28db8
vale75020 <valentinosuma@hotmail.com>
2019-11-04 17:10:33 +0100

    boolean

--- models/card.model.js
+++ models/card.model.js
@@ -1,27 +1,21 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const Mongoose = require("mongoose");
+const Joigoose = require("joigoose")(Mongoose);
+const Joi = require("joi");
 
-const cardSchema = new Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "users"
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  text: {
-    type: String,
-    required: true
-  },
-  status: {
-    type: String,
-    default: "to validate"
-  },
-  date: {
-    type: Date,
-    default: new Date()
-  }  
+var joiCardSchema = Joi.object({
+  user: Joi.string().meta({ type: "ObjectId", ref: "users" }),
+  title: Joi.string()
+    .min(4)
+    .max(100)
+    .required(),
+  text: Joi.string()
+    .min(4)
+    .max(500)
+    .required(),
+  status: Joi.string().meta({ default: "to validate" }),
+  date: Joi.string().meta({ default: new Date() })
 });
 
-module.exports = Card = mongoose.model("cards", cardSchema);
+var mongooseCardSchema = new Mongoose.Schema(Joigoose.convert(joiCardSchema));
+
+module.exports = User = Mongoose.model("cards", mongooseCardSchema);
--- models/user.model.js
+++ models/user.model.js
@@ -1,38 +1,20 @@
-const Mongoose = require('mongoose');
-const Joigoose = require('joigoose')(Mongoose);
-const Joi = require('joi');
-
+const Mongoose = require("mongoose");
+const Joigoose = require("joigoose")(Mongoose);
+const Joi = require("joi");
 
 var joiUserSchema = Joi.object({
-  username: Joi.string().min(4).max(20).required(),
-  password: Joi.string().min(4).max(30).required(),
-  admin: Joi.boolean(),
-  // access_token: [
-  //   Joi.string(),
-  //   Joi.number()
-  // ],
+  username: Joi.string()
+    .min(4)
+    .max(30)
+    .required(),
+  password: Joi.string()
+    .min(4)
+    .max(100)
+    .required(),
+  admin: Joi.boolean().meta({ default: false })
 });
 
 var mongooseUserSchema = new Mongoose.Schema(Joigoose.convert(joiUserSchema));
 
-module.exports = User = Mongoose.model('users', mongooseUserSchema);
-
-// const mongoose = require("mongoose");
-// const { Schema } = mongoose;
-
-// const userSchema = new Schema({
-//   username: {
-//     type: String,
-//     required: true
-//   },
-//   password: {
-//     type: String,
-//     required: true
-//   },
-//   admin: {
-//     type: Boolean,
-//     default: false
-//   }
-// });
-
-// module.exports = User = mongoose.model("users", userSchema);
+module.exports = User = Mongoose.model("users", mongooseUserSchema);
+ 
